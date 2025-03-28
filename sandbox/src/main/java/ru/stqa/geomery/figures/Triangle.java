fa9b9de996821550498a3f4606dc7aaa80f49cc8
alupar <36619247+alupar@users.noreply.github.com>
2025-03-28 21:06:15 +0300

    Задание №3: Потренироваться использовать ветвление и исключения

--- sandbox/src/main/java/ru/stqa/geomery/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geomery/figures/Triangle.java
@@ -2,6 +2,14 @@ package ru.stqa.geomery.figures;
 
 public record Triangle(double a, double b, double c) {
 
+    public Triangle {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalArgumentException("Длина стороны треугольника должна быть положительной");
+        }
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalArgumentException("Нарушено неравенство треугольника (сумма двух любых сторон должна быть не меньше третьей стороны)");
+        }
+    }
 
     public double area() {
         double sp = perimeter() / 2;
--- sandbox/src/test/java/ru/stqa/geomery/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geomery/figures/TriangleTests.java
@@ -7,13 +7,67 @@ public class TriangleTests {
 
     @Test
     void calcPerimeter() {
-        var t = new Triangle(1.35, 2.92, 5.1);
-        Assertions.assertEquals(9.37, t.perimeter());
+        var t = new Triangle(2.35, 2.92, 5.1);
+        Assertions.assertEquals(10.37, t.perimeter());
     }
 
     @Test
     void calcArea() {
         var t = new Triangle(20, 19, 30);
         Assertions.assertEquals("186,795", String.format("%.3f", t.area()));
+    }
+
+    @Test
+    void cannotCreateTreangleWithNegativeSideA() {
+        try {
+            new Triangle(-20, 19, 30);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
+    }
+
+    @Test
+    void cannotCreateTreangleWithNegativeSideB() {
+        try {
+            new Triangle(19, -20, 30);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
+    }
+
+    @Test
+    void cannotCreateTreangleWithNegativeSideC() {
+        try {
+            new Triangle(19, 22, -30);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
+    }
+
+    @Test
+    void cannotCreateTreangleWithZeroSideA() {
+        try {
+            new Triangle(0, 21, 30);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
+    }
+
+    @Test
+    void cannotCreateTreangleWithZeroSideB() {
+        try {
+            new Triangle(19, 0, 30);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
+    }
+
+    @Test
+    void cannotCreateTreangleWithZeroSideC() {
+        try {
+            new Triangle(19, 12, 0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
     }
 }
